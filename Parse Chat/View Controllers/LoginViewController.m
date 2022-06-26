e9874a83c0fb4e5c46465e51cee479fefd91effe
Marty Nodado <mdrnodado@gmail.com>
2022-06-25 21:02:41 -0700

    implemented login/register functionality
    - handled case when either text field is empty

--- Parse Chat/View Controllers/LoginViewController.m
+++ Parse Chat/View Controllers/LoginViewController.m
@@ -6,6 +6,7 @@
 //
 
 #import "LoginViewController.h"
+#import <Parse/Parse.h>
 
 @interface LoginViewController ()
 
@@ -29,8 +30,72 @@
 */
 
 - (IBAction)registerUser:(id)sender {
+    
+    // Check if fields are empty. If YES, do not continue to block
+    if ([self isEitherFieldEmpty:self.usernameTextField.text pass:self.passwordTextField.text]) {
+        [self showEmptyFieldAlert];
+        return;
+    }
+    
+    // initialize a user object
+    PFUser *newUser = [PFUser user];
+    
+    // set user properties
+    newUser.username = self.usernameTextField.text;
+//    newUser.email = self.emailField.text; // don't neet a email for this app
+    newUser.password = self.passwordTextField.text;
+    
+    // call sign up function on the object
+    [newUser signUpInBackgroundWithBlock:^(BOOL succeeded, NSError * error) {
+        if (error != nil) {
+            NSLog(@"Error: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User registered successfully");
+            
+            // manually segue to logged in view
+        }
+    }];
 }
 
 - (IBAction)loginUser:(id)sender {
+    
+    // Check if fields are empty. If YES, do not continue to block
+    if ([self isEitherFieldEmpty:self.usernameTextField.text pass:self.passwordTextField.text]) {
+        [self showEmptyFieldAlert];
+        return;
+    }
+    
+    NSString *username = self.usernameTextField.text;
+    NSString *password = self.passwordTextField.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            
+            // display view controller that needs to shown after successful login
+        }
+    }];
 }
+
+- (void) showEmptyFieldAlert {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"One of the text fields are empty!" message:nil preferredStyle:UIAlertControllerStyleAlert];
+    
+    // create an OK action
+    UIAlertAction *okAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        
+        // handle response here.
+    }];
+    // add the OK action to the alert controller
+    [alert addAction:okAction];
+    
+    [self presentViewController:alert animated:YES completion:^{}];
+}
+
+- (bool)isEitherFieldEmpty: (NSString *)user pass:(NSString *)pass {
+    bool val = ([user isEqual:@""] || [pass isEqual:@""]);
+    return val;
+}
+
 @end
