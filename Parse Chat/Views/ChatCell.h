59c34fd78803fa9edc258859bc29c9ae2d83264c
Marty Nodado <mdrnodado@gmail.com>
2022-06-25 21:38:15 -0700

    initialized chat vc and storyboard

---
+++ Parse Chat/Views/ChatCell.h
@@ -0,0 +1,16 @@
+//
+//  ChatCell.h
+//  Parse Chat
+//
+//  Created by Marthan Nodado on 6/25/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ChatCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Parse Chat/Views/ChatCell.m
@@ -0,0 +1,23 @@
+//
+//  ChatCell.m
+//  Parse Chat
+//
+//  Created by Marthan Nodado on 6/25/22.
+//
+
+#import "ChatCell.h"
+
+@implementation ChatCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
